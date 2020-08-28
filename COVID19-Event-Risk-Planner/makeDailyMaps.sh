f6334de3ab1d0cc3317e3c64b8fe03611fda2141
Radar O'Reilly <radar@mac.com>
2020-08-28 16:18:56 -0500

    Un-relativized path for makeDailyMaps.sh, as it made the cronjob fail.

--- COVID19-Event-Risk-Planner/makeDailyMaps.sh
+++ COVID19-Event-Risk-Planner/makeDailyMaps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 fname=$(date +%Y%m%d_%H%M%S)
-base="."
+base="/srv/shiny-server/COVID19-Event-Risk-Planner/makeDailyMaps"
 mkdir -p ${base}/daily_risk_map/${fname}
 cd ${base}/
 Rscript ${base}/makeDailyMaps.R $fname $1
